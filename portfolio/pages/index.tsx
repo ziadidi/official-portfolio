7f0ce44faea56024c43d22d0d6089f78de76a0ce
ziadidi <dianepucci19@gmail.com>
2022-09-29 17:19:00 +0100

    push

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -3,26 +3,31 @@ import Head from "next/head";
 import About from "../components/About";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
-import styles from "../styles/Home.module.css";
+import WorkExperience from "../components/WorkExperience";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory 
-    overflow-scroll z-0">
+    <div
+      className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory 
+    overflow-scroll z-0"
+    >
       <Head>
         <title>Diane's Portfolio</title>
       </Head>
 
       <Header />
-      
+
       <section id="hero" className="snap-start">
         <Hero />
       </section>
-      
+
       <section id="about" className="snap-center">
         <About />
       </section>
-      {/* Experience */}
+
+      <section id="experience" className="snap-center">
+        <WorkExperience />
+      </section>
       {/* Skills */}
       {/* Projects */}
       {/* Contact Me */}
