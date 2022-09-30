b04379e7876cbd574c554467a742e2d8a99afd31
ziadidi <dianepucci19@gmail.com>
2022-09-30 23:30:21 +0100

    cool

---
+++ portfolio/components/ExperienceCard.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+type Props = {};
+
+function ExperienceCard({}: Props) {
+  return (
+    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0
+    w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100
+    opacity-40">
+      <motion.img
+        initial={{
+          y: -100,
+          opacity: 0,
+        }}
+        transition={{ duration: 1.2 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        className="w-32 h-32 rounded-full xl:w-[200px] object-cover
+        object-center"
+        src="https://i.postimg.cc/28CyHfmQ/logo.png"
+        alt=""
+      />
+
+      <div className="px-0 md:px-10">
+        <h4 className="text-4xl font-light">Freelance Web Developers</h4>
+        <p className="font-bold text-2xxl mt-1">ASDP devs</p>
+        <div className="flex space-x-2 my-2">
+          <img
+            src="http://s3.amazonaws.com/gt7sp-prod/decal/20/89/27/5125114039554278920_1.png"
+            alt=""
+            className="h-10 w-10 rounded-full object-cover"
+          />
+          <img
+            src="http://s3.amazonaws.com/gt7sp-prod/decal/20/89/27/5125114039554278920_1.png"
+            alt=""
+            className="h-10 w-10 rounded-full object-cover"
+          />
+
+          <img
+            src="http://s3.amazonaws.com/gt7sp-prod/decal/20/89/27/5125114039554278920_1.png"
+            alt=""
+            className="h-10 w-10 rounded-full object-cover"
+          />
+        </div>
+
+        <p className="uppercase py-5 text-gray-300">
+          Started work... - Ended...
+        </p>
+
+        <ul className="list-disc space-y-4 ml-5 text-lg">
+          <li>Summary points</li>
+          <li>Summary points</li>
+          <li>Summary points</li>
+          <li>Summary points</li>
+          <li>Summary points</li>
+        </ul>
+      </div>
+    </article>
+  );
+}
+
+export default ExperienceCard;
--- portfolio/components/WorkExperience.tsx
+++ portfolio/components/WorkExperience.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { motion } from "framer-motion";
+import ExperienceCard from "./ExperienceCard";
 
 type Props = {};
 
 function WorkExperience({}: Props) {
   return (
-    <div
+    <motion.div
+    initial={{ opacity: 0 }}
+    whileInView={{ opacity: 1 }}
+    transition={{ duration: 1.5 }}
       className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row
      max-w-full px-10 justify-evenly mx-auto items-center"
     >
@@ -14,7 +19,14 @@ function WorkExperience({}: Props) {
       >
         Experince
       </h3>
-    </div>
+
+      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x 
+      snap-mandatory">
+        <ExperienceCard />
+        <ExperienceCard />
+        <ExperienceCard />
+      </div>
+    </motion.div>
   );
 }
 
